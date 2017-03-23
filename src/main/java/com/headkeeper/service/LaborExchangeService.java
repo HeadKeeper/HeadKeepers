a450f87aef096fd9b322cfbd39f254b3b419561c
justandreyb <justandreyb@gmail.com>
2017-03-23 18:48:25 +0300

    Create service for CRUD

---
+++ src/main/java/com/headkeeper/service/LaborExchangeService.java
@@ -0,0 +1,157 @@
+package com.headkeeper.service;
+
+import com.headkeeper.bean.*;
+
+import java.util.List;
+
+public interface LaborExchangeService {
+
+    /* -------------------- Vacancy -------------------- */
+
+    void createVacancy(Vacancy vacancy);
+
+    void updateVacancy(int id, Vacancy vacancy);
+
+    Vacancy getVacancy(int id);
+
+    List<Vacancy> getAllVacancies();
+    List<Vacancy> getVacanciesForEmployer(int employerId);
+
+    void setVacancyActiveStatus(int id, boolean status);
+
+    void deleteVacancy(int id);
+
+    /* ------------------------------------------------- */
+
+
+    /* -------------------- Resume --------------------- */
+
+    void createResume(UserResume resume);
+
+    void updateResume(int resumeId, UserResume resume);
+
+    UserResume getResume(int resumeId);
+
+    List<UserResume> getResumesForUser(int userId);
+
+    void setResumeStatus(int resumeId, boolean status);
+
+    void deleteResume(int resumeId);
+
+    /* ------------------------------------------------- */
+
+
+    /* --------------------- Skill --------------------- */
+
+    void createSkill(Skill skill);
+
+    void updateSkill(int skillId, Skill skill);
+
+    List<Skill> getSkillsForUser(int userId);
+    List<Skill> getSkillsForVacancy(int vacancyId);
+
+    void deleteSkill(int skillId);
+
+    /* ------------------------------------------------- */
+
+
+    /* ------------------ Certificate ------------------ */
+
+    void uploadCertificate(UserCertificate certificate);
+
+    List<UserCertificate> getCetificatesForUser(int userId);
+
+    void deleteCertificate(int certificateId);
+
+    /* ------------------------------------------------- */
+
+
+    /* ------------------ Achievement ------------------ */
+
+    void addAchievement(ResumeAchievement achievement);
+
+    void updateAchievement(int achievementId, ResumeAchievement achievement);
+
+    List<ResumeAchievement> getAchievementsForResume(int resumeId);
+
+    void deleteAchievement(int achievementId);
+
+    /* ------------------------------------------------- */
+
+
+    /* ------------------- Education ------------------- */
+
+    void addEducation(ResumeEducation education);
+
+    void updateEducation(int educationId, ResumeEducation education);
+
+    List<ResumeEducation> getEducationsForResume(int resumeId);
+
+    void deleteEducation(int educationId);
+
+    /* ------------------------------------------------- */
+
+
+    /* ------------- Additional education -------------- */
+
+    void addAdditionalEducation(ResumeAdditionalEducation education);
+
+    void updateAdditionalEducation(int educationId, ResumeAdditionalEducation education);
+
+    List<ResumeAdditionalEducation> getAdditionalEducationsForResume(int resumeId);
+
+    void deleteAdditionalEducation(int educationId);
+
+    /* ------------------------------------------------- */
+
+
+    /* ----------------- Contact info ------------------ */
+
+    void addContactInfo(ResumeContactInfo contactInfo);
+
+    void updateContactInfo(int contactInfoId, ResumeContactInfo contactInfo);
+
+    List<ResumeContactInfo> getContactInfosForResume(int resumeId);
+
+    void deleteContactInfo(int contactInfoId);
+
+    /* ------------------------------------------------- */
+
+
+    /* ------------------- Language -------------------- */
+
+    void addLanguage(ResumeLanguage language);
+
+    void updateLanguage(int languageId, ResumeLanguage language);
+
+    List<ResumeContactInfo> getLanguagesForResume(int resumeId);
+
+    void deleteLanguage(int languageId);
+
+    /* ------------------------------------------------- */
+
+
+    /* --------------------- Photo --------------------- */
+
+    void uploadPhoto(ResumePhoto photo);
+
+    List<ResumePhoto> getPhotosForResume(int resumeId);
+
+    void deletePhoto(int photoId);
+
+    /* ------------------------------------------------- */
+
+
+    /* ---------------- Work experience ---------------- */
+
+    void addWorkExperience(ResumeWorkExperience workExperience);
+
+    void updateWorkExperience(int workExperienceId, ResumeWorkExperience workExperience);
+
+    List<ResumeWorkExperience> getWorkExperiencesForResume(int resumeId);
+
+    void deleteWorkExperience(int workExperienceId);
+
+    /* ------------------------------------------------- */
+
+}
---
+++ src/main/java/com/headkeeper/service/impl/LaborExchangeServiceImpl.java
@@ -0,0 +1,256 @@
+package com.headkeeper.service.impl;
+
+import com.headkeeper.bean.*;
+import com.headkeeper.service.LaborExchangeService;
+
+import java.util.List;
+
+public class LaborExchangeServiceImpl implements LaborExchangeService {
+
+    /* -------------------- Vacancy -------------------- */
+
+    public void createVacancy(Vacancy vacancy) {
+
+    }
+
+    public void updateVacancy(int id, Vacancy vacancy) {
+
+    }
+
+    public Vacancy getVacancy(int id) {
+        return null;
+    }
+
+    public List<Vacancy> getAllVacancies() {
+        return null;
+    }
+
+    public List<Vacancy> getVacanciesForEmployer(int employerId) {
+        return null;
+    }
+
+    public void setVacancyActiveStatus(int id, boolean status) {
+
+    }
+
+    public void deleteVacancy(int id) {
+
+    }
+
+    /* ------------------------------------------------- */
+
+
+    /* -------------------- Resume --------------------- */
+
+    public void createResume(UserResume resume) {
+
+    }
+
+    public void updateResume(int resumeId, UserResume resume) {
+
+    }
+
+    public UserResume getResume(int resumeId) {
+        return null;
+    }
+
+    public List<UserResume> getResumesForUser(int userId) {
+        return null;
+    }
+
+    public void setResumeStatus(int resumeId, boolean status) {
+
+    }
+
+    public void deleteResume(int resumeId) {
+
+    }
+
+    /* ------------------------------------------------- */
+
+
+    /* --------------------- Skill --------------------- */
+
+    public void createSkill(Skill skill) {
+
+    }
+
+    public void updateSkill(int skillId, Skill skill) {
+
+    }
+
+    public List<Skill> getSkillsForUser(int userId) {
+        return null;
+    }
+
+    public List<Skill> getSkillsForVacancy(int vacancyId) {
+        return null;
+    }
+
+    public void deleteSkill(int skillId) {
+
+    }
+
+    /* ------------------------------------------------- */
+
+
+    /* ------------------ Certificate ------------------ */
+
+    public void uploadCertificate(UserCertificate certificate) {
+
+    }
+
+    public List<UserCertificate> getCetificatesForUser(int userId) {
+        return null;
+    }
+
+    public void deleteCertificate(int certificateId) {
+
+    }
+
+    /* ------------------------------------------------- */
+
+
+    /* ------------------ Achievement ------------------ */
+
+    public void addAchievement(ResumeAchievement achievement) {
+
+    }
+
+    public void updateAchievement(int achievementId, ResumeAchievement achievement) {
+
+    }
+
+    public List<ResumeAchievement> getAchievementsForResume(int resumeId) {
+        return null;
+    }
+
+    public void deleteAchievement(int achievementId) {
+
+    }
+
+    /* ------------------------------------------------- */
+
+
+    /* ------------------- Education ------------------- */
+
+    public void addEducation(ResumeEducation education) {
+
+    }
+
+    public void updateEducation(int educationId, ResumeEducation education) {
+
+    }
+
+    public List<ResumeEducation> getEducationsForResume(int resumeId) {
+        return null;
+    }
+
+    public void deleteEducation(int educationId) {
+
+    }
+
+    /* ------------------------------------------------- */
+
+
+    /* ------------- Additional education -------------- */
+
+    public void addAdditionalEducation(ResumeAdditionalEducation education) {
+
+    }
+
+    public void updateAdditionalEducation(int educationId, ResumeAdditionalEducation education) {
+
+    }
+
+    public List<ResumeAdditionalEducation> getAdditionalEducationsForResume(int resumeId) {
+        return null;
+    }
+
+    public void deleteAdditionalEducation(int educationId) {
+
+    }
+
+    /* ------------------------------------------------- */
+
+
+    /* ----------------- Contact info ------------------ */
+
+    public void addContactInfo(ResumeContactInfo contactInfo) {
+
+    }
+
+    public void updateContactInfo(int contactInfoId, ResumeContactInfo contactInfo) {
+
+    }
+
+    public List<ResumeContactInfo> getContactInfosForResume(int resumeId) {
+        return null;
+    }
+
+    public void deleteContactInfo(int contactInfoId) {
+
+    }
+
+    /* ------------------------------------------------- */
+
+
+    /* ------------------- Language -------------------- */
+
+    public void addLanguage(ResumeLanguage language) {
+
+    }
+
+    public void updateLanguage(int languageId, ResumeLanguage language) {
+
+    }
+
+    public List<ResumeContactInfo> getLanguagesForResume(int resumeId) {
+        return null;
+    }
+
+    public void deleteLanguage(int languageId) {
+
+    }
+
+    /* ------------------------------------------------- */
+
+
+    /* --------------------- Photo --------------------- */
+
+    public void uploadPhoto(ResumePhoto photo) {
+
+    }
+
+    public List<ResumePhoto> getPhotosForResume(int resumeId) {
+        return null;
+    }
+
+    public void deletePhoto(int photoId) {
+
+    }
+
+    /* ------------------------------------------------- */
+
+
+    /* ---------------- Work experience ---------------- */
+
+    public void addWorkExperience(ResumeWorkExperience workExperience) {
+
+    }
+
+    public void updateWorkExperience(int workExperienceId, ResumeWorkExperience workExperience) {
+
+    }
+
+    public List<ResumeWorkExperience> getWorkExperiencesForResume(int resumeId) {
+        return null;
+    }
+
+    public void deleteWorkExperience(int workExperienceId) {
+
+    }
+
+    /* ------------------------------------------------- */
+}
+
