b6f4101ce401ccb4a28bcf547e8d9aba60aa4c4c
Maxim Petrusevich <maks1464@gmail.com>
2017-04-03 22:25:33 +0300

    + EmployerInfo and CompanyInfo DAO methods in UserDAO

--- src/main/java/com/headkeeper/dao/UserDAO.java
+++ src/main/java/com/headkeeper/dao/UserDAO.java
@@ -1,23 +1,35 @@
 package com.headkeeper.dao;
 
+import com.headkeeper.bean.entity.CompanyInfo;
+import com.headkeeper.bean.entity.EmployerInfo;
 import com.headkeeper.bean.entity.User;
 import com.headkeeper.dao.exception.DAOException;
 
+import java.util.List;
+
 public interface UserDAO {
-    static final int USER_ROLE_ID = 1;
     /* CRUD operations */
 
     // CREATE
-    void addNewUser(User user) throws DAOException;
+    void addNewUser(User user, int roleId) throws DAOException;
+    void addCompanyInfo(CompanyInfo companyInfo, int ownerId) throws DAOException;
+    void addEmployerInfo(EmployerInfo employerInfo, int userId) throws DAOException;
 
     // READ
     Object getUserById(int id) throws DAOException;
+    CompanyInfo getCompanyInfoById(int id) throws DAOException;
+    EmployerInfo getEmployerInfoById(int id) throws DAOException;
+    List<User> getUsers() throws DAOException;
 
     // UPDATE
     void updateUser(int id, User user) throws DAOException;
     void updateUser(int id, boolean status) throws DAOException;
+    void updateCompanyInfo(int id, CompanyInfo companyInfo) throws DAOException;
+    void updateEmployerInfo(int id, EmployerInfo employerInfo) throws DAOException;
 
     // DELETE
     void deleteUser(int id) throws DAOException;
+    void deleteCompanyInfo(int id) throws DAOException;
+    void deleteEmployerInfo(int id) throws DAOException;
 
 }
--- src/main/java/com/headkeeper/dao/impl/UserDAOImpl.java
+++ src/main/java/com/headkeeper/dao/impl/UserDAOImpl.java
@@ -1,5 +1,7 @@
 package com.headkeeper.dao.impl;
 
+import com.headkeeper.bean.entity.CompanyInfo;
+import com.headkeeper.bean.entity.EmployerInfo;
 import com.headkeeper.bean.entity.Role;
 import com.headkeeper.bean.entity.User;
 import com.headkeeper.dao.UserDAO;
@@ -40,14 +42,35 @@ public class UserDAOImpl implements UserDAO {
         }
     }
 
+    private void checkCompanyInfoOnExist(CompanyInfo companyInfo, Session session) throws HibernateException {
+        /*
+            Here we make a request to database through proxy, which we get from session.load
+            If Company Info doesn't exist, hibernate throw exception
+            If Company Info exist - hibernate execute query
+        */
+        String query = "from CompanyInfo where id = " + companyInfo.getId();
+        List resultList = session.createQuery(query).list();
+        if (resultList.size() == 0) {
+            throw new HibernateException("Can't find company with id = " + companyInfo.getId());
+        }
+    }
+
+    private void checkEmployerInfoOnExist(EmployerInfo employerInfo, Session session) {
+        String query = "from EmployerInfo where id = " + employerInfo.getId();
+        List resultList = session.createQuery(query).list();
+        if (resultList.size() == 0) {
+            throw new HibernateException("Can't find employer info with id = " + employerInfo.getId());
+        }
+    }
+
     /* CRUD OPERATIONS */
 
     // ------------------------------- CREATE -------------------------------
 
-    public void addNewUser(User user) throws DAOException {
+    public void addNewUser(User user, int roleId) throws DAOException {
         try {
             Session session = sessionFactory.getCurrentSession();
-            Role role = session.load(Role.class, USER_ROLE_ID);
+            Role role = session.load(Role.class, roleId);
             user.setRole(role);
             session.save(user);
         }
@@ -56,6 +79,38 @@ public class UserDAOImpl implements UserDAO {
         }
         catch (HibernateException exception) {
             throw new DAOException("Can't add user to database. This user already exist.");
+        }
+    }
+
+    public void addCompanyInfo(CompanyInfo companyInfo, int ownerId) throws DAOException {
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            User user = session.load(User.class, ownerId);
+            checkUserOnExist(user, session);
+            companyInfo.setUser(user);
+            session.save(companyInfo);
+        }
+        catch (SessionException exception) {
+            throw new DAOException("Can't get current session.");
+        }
+        catch (HibernateException exception) {
+            throw new DAOException("Can't add company info to database. This company already exist.");
+        }
+    }
+
+    public void addEmployerInfo(EmployerInfo employerInfo, int userId) throws DAOException {
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            User user = session.load(User.class, userId);
+            checkUserOnExist(user, session);
+            employerInfo.setUser(user);
+            session.save(employerInfo);
+        }
+        catch (SessionException exception) {
+            throw new DAOException("Can't get current session.");
+        }
+        catch (HibernateException exception) {
+            throw new DAOException("Can't add employer info to database. This employer already exist.");
         }
     }
 
@@ -74,6 +129,51 @@ public class UserDAOImpl implements UserDAO {
             throw new DAOException("Can't get current session.");
         }
     }
+
+
+    public CompanyInfo getCompanyInfoById(int id) throws DAOException {
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            CompanyInfo companyInfo = session.get(CompanyInfo.class, id);
+            if (companyInfo == null) {
+                throw new DAOException("Can't find company info with id = " + id);
+            }
+            return companyInfo;
+        }
+        catch (SessionException exception) {
+            throw new DAOException("Can't get current session.");
+        }
+    }
+
+
+    public EmployerInfo getEmployerInfoById(int id) throws DAOException {
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            EmployerInfo employerInfo = session.get(EmployerInfo.class, id);
+            if (employerInfo == null) {
+                throw new DAOException("Can't find employer info with id = " + id);
+            }
+            return employerInfo;
+        }
+        catch (SessionException exception) {
+            throw new DAOException("Can't get current session.");
+        }
+    }
+
+    public List<User> getUsers() throws DAOException {
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            List<User> users = (List<User>) session.createQuery("from User").list();
+            return users;
+        }
+        catch (SessionException exception) {
+            throw new DAOException("Can't get current session");
+        }
+        catch (HibernateException exception) {
+            throw new DAOException("Error in HQL query execution.");
+        }
+    }
+
 
     // ------------------------------- UPDATE -------------------------------
 
@@ -111,12 +211,48 @@ public class UserDAOImpl implements UserDAO {
         }
     }
 
+    public void updateCompanyInfo(int id, CompanyInfo companyInfo) throws DAOException {
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            CompanyInfo oldCompanyInfo = session.load(CompanyInfo.class, id);
+            checkCompanyInfoOnExist(companyInfo, session);
+            oldCompanyInfo.setName(companyInfo.getName());
+            oldCompanyInfo.setAddress(companyInfo.getAddress());
+            oldCompanyInfo.setDescription(companyInfo.getDescription());
+            session.update(oldCompanyInfo);
+        }
+        catch (SessionException exception) {
+            throw new DAOException("Can't get current session.");
+        }
+        catch (HibernateException exception) {
+            throw new DAOException("Company with id = " + id +" not found.");
+        }
+    }
+
+    public void updateEmployerInfo(int id, EmployerInfo employerInfo) throws DAOException {
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            EmployerInfo oldEmployerInfo = session.load(EmployerInfo.class, id);
+            checkEmployerInfoOnExist(oldEmployerInfo, session);
+            oldEmployerInfo.setName(employerInfo.getName());
+            oldEmployerInfo.setPhoneNumber(employerInfo.getPhoneNumber());
+            session.update(oldEmployerInfo);
+        }
+        catch (SessionException exception) {
+            throw new DAOException("Can't get current session.");
+        }
+        catch (HibernateException exception) {
+            throw new DAOException("Employer with id = " + id +" not found.");
+        }
+    }
+
+
     // ------------------------------- DELETE -------------------------------
 
     public void deleteUser(int id) throws DAOException {
         try {
             Session session = sessionFactory.getCurrentSession();
-            User user = (User) session.load(User.class, id);
+            User user = session.load(User.class, id);
             checkUserOnExist(user, session);
             session.delete(user);
         }
@@ -125,6 +261,36 @@ public class UserDAOImpl implements UserDAO {
         }
         catch (HibernateException exception) {
             throw new DAOException("User with id = " + id +" not found.");
+        }
+    }
+
+    public void deleteCompanyInfo(int id) throws DAOException {
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            CompanyInfo companyInfo = session.load(CompanyInfo.class, id);
+            checkCompanyInfoOnExist(companyInfo, session);
+            session.delete(companyInfo);
+        }
+        catch (SessionException exception) {
+            throw new DAOException("Can't get current session.");
+        }
+        catch (HibernateException exception) {
+            throw new DAOException("Company with id = " + id +" not found.");
+        }
+    }
+
+    public void deleteEmployerInfo(int id) throws DAOException {
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            EmployerInfo employerInfo = session.load(EmployerInfo.class, id);
+            checkEmployerInfoOnExist(employerInfo, session);
+            session.delete(employerInfo);
+        }
+        catch (SessionException exception) {
+            throw new DAOException("Can't get current session.");
+        }
+        catch (HibernateException exception) {
+            throw new DAOException("Employer with id = " + id +" not found.");
         }
     }
 
